c05cf1631d132ee35c30532c3e7aeba578dc612e
simoleans <francisco20990@gmail.com>
2016-07-13 20:35:21 -0400

    ss
    sss

--- siot/protected/views/seguimiento/Partials/Mensaje_incorrecto.php
+++
@@ -1,6 +0,0 @@
-<div class='alert alert-danger alert-dismissible' role='alert'   align='center'>
-<button type='button' class='close' data-dismiss='alert' aria-label='Close'>
-		<span aria-hidden='true'>&times;</span>
-</button>
-		<b style='color:#A32121; text-transform: uppercase;'>Elemento Incorrecto</b>!
-</div>
